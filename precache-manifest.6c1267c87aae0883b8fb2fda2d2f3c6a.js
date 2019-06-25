d49f950e9bdfada7975706426559a6f14adcbede
Abhishek Pandya <abhisekpandya5@gmail.com>
2019-06-25 11:37:43 +0530

    Updates

---
+++ precache-manifest.6c1267c87aae0883b8fb2fda2d2f3c6a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "da907d8b62992d600bd3e691611318cb",
+    "url": "/React_Meteor_Bootstrap/index.html"
+  },
+  {
+    "revision": "18ec2828cfb383ced5da",
+    "url": "/React_Meteor_Bootstrap/static/css/main.0b7a07dc.chunk.css"
+  },
+  {
+    "revision": "d1e1bd2816cfcf97374c",
+    "url": "/React_Meteor_Bootstrap/static/js/2.001fab56.chunk.js"
+  },
+  {
+    "revision": "18ec2828cfb383ced5da",
+    "url": "/React_Meteor_Bootstrap/static/js/main.6634ec68.chunk.js"
+  },
+  {
+    "revision": "aee2a294dcc60585ade6",
+    "url": "/React_Meteor_Bootstrap/static/js/runtime~main.62f34ed9.js"
+  }
+]);
