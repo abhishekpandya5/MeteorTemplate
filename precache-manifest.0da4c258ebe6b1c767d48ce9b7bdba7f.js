70503dfc8317310b4a8ed96d396e7ce40ec71322
Abhishek Pandya <abhishek.pandya@tothenew.com>
2019-10-04 11:50:44 +0530

    Updates

---
+++ precache-manifest.0da4c258ebe6b1c767d48ce9b7bdba7f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3b4763fb6a547e553418364e6cb55f1a",
+    "url": "/MeteorTemplate/index.html"
+  },
+  {
+    "revision": "c161cba36d9d17ad836d",
+    "url": "/MeteorTemplate/static/css/main.0b7a07dc.chunk.css"
+  },
+  {
+    "revision": "d1e1bd2816cfcf97374c",
+    "url": "/MeteorTemplate/static/js/2.001fab56.chunk.js"
+  },
+  {
+    "revision": "c161cba36d9d17ad836d",
+    "url": "/MeteorTemplate/static/js/main.3e48f34f.chunk.js"
+  },
+  {
+    "revision": "964247a4bd09af058650",
+    "url": "/MeteorTemplate/static/js/runtime~main.6aa8e69a.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/React_Meteor_Bootstrap/precache-manifest.6c1267c87aae0883b8fb2fda2d2f3c6a.js"
+  "/MeteorTemplate/precache-manifest.0da4c258ebe6b1c767d48ce9b7bdba7f.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/React_Meteor_Bootstrap/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/MeteorTemplate/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
